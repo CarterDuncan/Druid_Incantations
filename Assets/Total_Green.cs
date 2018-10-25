6524e1c4ca7220400fab2bd2fb43c4eebb1cc49a
Luke <scu.imaginarium@gmail.com>
2018-10-25 09:58:58 -0700

    green objects scripts

---
+++ Assets/Total_Green.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+public class Total_Green : MonoBehaviour {
+    static int total_green;
+
+	// Use this for initialization
+	void Start () {
+        total_green = 0;
+        public int object;
+        Debug.Log("I work");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Instantiate(green))
+            total_green++;
+
+        if (total_green > 0 && total_green < 25)
+            Terrain.texture = "dull_green";
+        if (total_green > 25 && total_green < 50)
+            Terrain.texture = "light_green";
+        if (total_green > 50 && total_green < 75)
+            Terrain.texture = "dark_green";
+}//create a list that will add an object to the list every timem its instantiteated
+//counter == size of list 
+}//https://answers.unity.com/questions/996317/how-add-values-to-genericlist.html
+//https://answers.unity.com/questions/318607/how-to-access-the-size-of-a-list-c.html
+*/
