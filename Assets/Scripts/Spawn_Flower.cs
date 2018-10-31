c8bce7f5f3f0eaa4ba803bb1379079edbdcf4865
Luke <scu.imaginarium@gmail.com>
2018-10-31 16:49:36 -0700

    added colliders

---
+++ Assets/Scripts/Spawn_Flower.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_Flower : MonoBehaviour {
+
+    public int flower_count;
+
+	// Use this for initialization
+	void Start () {
+        flower_count = 0;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+            Debug.Log("Button Pressed");
+            SpawnFlower();
+        }
+	}
+
+    void SpawnFlower()
+    {
+        Debug.Log("Flower Spawned");
+    }
+}
