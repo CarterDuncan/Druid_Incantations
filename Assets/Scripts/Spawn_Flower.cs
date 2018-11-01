628635a9a84b97184fd0ed8533f91a25403fe456
Luke <scu.imaginarium@gmail.com>
2018-10-31 20:43:00 -0700

    scripts

--- Assets/Scripts/Spawn_Flower.cs
+++ Assets/Scripts/Spawn_Flower.cs
@@ -5,11 +5,26 @@ using UnityEngine;
 public class Spawn_Flower : MonoBehaviour {
 
     public int flower_count;
-    public GameObject sunflower;
+    public Transform sunflower;
+    Rigidbody plant;
+
+    //number of elements to be initiated 
+    public int NumElements = 5;
+    //the transform object to be initiated
+    public Transform prefab;
+
+    //the controllable player
+    public GameObject Player;
+    //how far away from player
+    private float DistFromPlayer = 10.0f;
+    //how many elements have been created
+    private int generatedElementsCount = 0;
 
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
+        Player = GameObject.Find("Player");
         flower_count = 0;
 		
 	}
@@ -21,19 +36,66 @@ public class Spawn_Flower : MonoBehaviour {
         {
             Debug.Log("Button Pressed");
             //Instantiate
-            SpawnFlower();
+            //SpawnFlower();
+            //SpawnPlant
+            CreateElements();
         }
         //if A released this frame
         if (OVRInput.GetUp(OVRInput.RawButton.X))
         {
             Debug.Log("Button released");
-     
+            flower_count++;
         }
 	}
-
+    //creats a volly of flowers at origin!?! Need to fix position
     void SpawnFlower()
     {
         Debug.Log("Flower Spawned");
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(prefab, new Vector3(i * 2.0F, 0, 0), Quaternion.identity);
+        }
+    }
+    //haven't tessted but idea to create and launch flower to floor like projectile, will need to tweak velocity so lands perfectly
+    void SpawnPlant()
+    {
+        // Ctrl was pressed, launch a projectile
+        if (Input.GetButtonDown("Fire1"))
+        {
+            // Instantiate the projectile at the position and rotation of this transform
+            Rigidbody clone;
+            clone = Instantiate(plant, transform.position, transform.rotation);
+
+            // Give the cloned object an initial velocity along the current
+            // object's Z axis
+            clone.velocity = transform.TransformDirection(Vector3.forward * 10);
+        }
+    }
+    void CreateElements()
+    {
+        Debug.Log("Entered Function");
+        for (int i = 0; i < NumElements; i++)
+        {
+            Debug.Log("Entered Loop");
+            //update total elements created
+            generatedElementsCount++;
+            //name of the instatntiated object
+            string objectName = "Element_" + generatedElementsCount;
+            //angle of instantiated object
+            float angleIteration = 360 / NumElements;
+            //rotation of instantiation object
+            float currentRotation = angleIteration * i;
+
+            //create the object as a transform
+            Transform elem;
+            elem = Instantiate(sunflower, Player.transform.position, Player.transform.rotation) as Transform;
+
+            elem.name = objectName;
+
+            //update the position and rotation of the object
+            elem.transform.Rotate(new Vector3(0, currentRotation, 0));
+            elem.transform.Translate(new Vector3(DistFromPlayer, 5, 0));
+        }
     }
 
 }
--- Assets/Scripts/Total_Green.cs
+++ Assets/Scripts/Total_Green.cs
@@ -1,30 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-/*
+
 public class Total_Green : MonoBehaviour {
     static int total_green;
+    public GameObject sunflower;
+    public TerrainData terrainData;
+    //public Component GetComponent(Type type);
 
 	// Use this for initialization
 	void Start () {
         total_green = 0;
-        public int object;
         Debug.Log("I work");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Instantiate(green))
+        if (Instantiate(sunflower))
             total_green++;
+        //ChangeTerrainColor(terrainData);
+    }
+    /*public void ChangeTerrainColor(TerrainData terrainData) {
+    Renderer rend = Terrain.getComponenet<Renderer>();
+     if (total_green > 0 && total_green < 25)
+         rend.material.color = new Vector4(0, .1f, .5f, .7f);
+     if (total_green > 25 && total_green < 50)
+          rend.material.color = new Vector4(0, .1f, .5f, .7f);
+      if (total_green > 50 && total_green < 75)
+          rend.material.color = new Vector4(0, .1f, .5f, .7f);
+    }*/
+}
 
-        if (total_green > 0 && total_green < 25)
-            Terrain.texture = "dull_green";
-        if (total_green > 25 && total_green < 50)
-            Terrain.texture = "light_green";
-        if (total_green > 50 && total_green < 75)
-            Terrain.texture = "dark_green";
-}//create a list that will add an object to the list every timem its instantiteated
+
+//create a list that will add an object to the list every timem its instantiteated
 //counter == size of list 
-}//https://answers.unity.com/questions/996317/how-add-values-to-genericlist.html
+//https://answers.unity.com/questions/996317/how-add-values-to-genericlist.html
 //https://answers.unity.com/questions/318607/how-to-access-the-size-of-a-list-c.html
-*/
+
+
