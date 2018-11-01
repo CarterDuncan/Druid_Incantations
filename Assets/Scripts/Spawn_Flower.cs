631c0806cd00e61fe6b9e775b7c53a56991f22b9
Luke <scu.imaginarium@gmail.com>
2018-10-31 17:19:19 -0700

    scripts

--- Assets/Scripts/Spawn_Flower.cs
+++ Assets/Scripts/Spawn_Flower.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Spawn_Flower : MonoBehaviour {
 
     public int flower_count;
+    public GameObject sunflower;
+
 
 	// Use this for initialization
 	void Start () {
@@ -14,10 +16,18 @@ public class Spawn_Flower : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        //if "A" pressed this frame
 		if (OVRInput.GetDown(OVRInput.Button.One))
         {
             Debug.Log("Button Pressed");
+            //Instantiate
             SpawnFlower();
+        }
+        //if A released this frame
+        if (OVRInput.GetUp(OVRInput.RawButton.X))
+        {
+            Debug.Log("Button released");
+     
         }
 	}
 
@@ -25,4 +35,5 @@ public class Spawn_Flower : MonoBehaviour {
     {
         Debug.Log("Flower Spawned");
     }
+
 }
