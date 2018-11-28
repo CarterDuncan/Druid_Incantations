48afb9605ebdee2836734d9b6ca11a58ed786dfd
Luke <scu.imaginarium@gmail.com>
2018-11-28 15:17:22 -0800

    UI (WIP)

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class ChangeScene: MonoBehaviour {
+    public GameObject Player;
+    void Start()
+    {
+        Player = GameObject.Find("LeftHandAnchor");
+    }
+
+        void Update()
+    {
+        //if "A" pressed this frame
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+            changeScene();
+        }
+    }
+
+            public void changeScene()
+    {
+        SceneManager.LoadScene("MyScene");
+    }
+
+	
+}
